233089db5c7bdf5b86ed5f50b116dfa292466074
ShreyasReddy007 <podduturishreyasreddy@gmail.com>
2025-03-25 17:35:01 +0530

    Improved Register Functionality

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,13 +1,65 @@
-import React from "react";
+import React, {useState,useEffect}from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg"
+import {ToastContainer,toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoutes";
 function Register(){
+    const [values,setValues]=useState({
+      username:"",
+      email:"",
+      password:"",
+      confirmPassword:"",
+    });
+    const toastOptions={
+      position: "bottom-right",
+      autoClose:8000,
+      pauseOnHover:true,
+      draggable:true,
+      theme:"dark",
+    };
     const handleSubmit = (event) =>{
         event.preventDefault();
-        alert("form");
+        if(handleValidation()){
+          const { password, confirmPassword, username, email } = values;
+          const{data} =  axios.post(registerRoute,{
+            username,
+            email,
+            password,
+          });
+        }
     };
-    const handleChange = (event)=>{};
+    const handleValidation =(event)=>{
+      const { password, confirmPassword, username, email } = values;
+      if (password !== confirmPassword) {
+        toast.error(
+          "Password and confirm password should be same.",
+          toastOptions
+        );
+        return false;
+      } else if (username.length < 3) {
+        toast.error(
+          "Username should be greater than 3 characters.",
+          toastOptions
+        );
+        return false;
+      } else if (password.length < 8) {
+        toast.error(
+          "Password should be equal or greater than 8 characters.",
+          toastOptions
+        );
+        return false;
+      } else if (email === "") {
+        toast.error("Email is required.", toastOptions);
+        return false;
+      }
+      return true;
+    };
+    const handleChange = (event)=>{
+      setValues({ ...values,[event.target.name]:event.target.value});
+    };
     return(
         <>
         <FormContainer>
@@ -46,6 +98,7 @@ function Register(){
             </span>
            </form>
         </FormContainer>
+        <ToastContainer/>
         </>
     )
 }
