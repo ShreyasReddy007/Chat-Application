0fbc35a290c5fc78c75282b96c43c5572083d519
ShreyasReddy007 <podduturishreyasreddy@gmail.com>
2025-07-23 21:31:19 +0530

    Added Chat Page along with Contact Functionality

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,8 +1,67 @@
-import React from "react"
+// See later about the localStorage part where we might get some trouble.
+
+import React,{useState,useEffect} from "react";
+import styled from "styled-components";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 function Chat(){
-    return(
-        <div>Chat</div>
-    )
+    const navigate = useNavigate();
+    const socket = useRef();
+    const [contacts, setContacts] = useState([]);
+    const [currentChat, setCurrentChat] = useState(undefined);
+    const [currentUser, setCurrentUser] = useState(undefined);
+
+    useEffect(async () => {
+        if (!localStorage.getItem("chat-app-user")) {
+          navigate("/login");
+        } else {
+          setCurrentUser(
+            await JSON.parse(
+              localStorage.getItem("chat-app-user")
+            )
+          );
+        }
+      }, []);
+
+    useEffect(async () => {
+        if (currentUser) {
+          if (currentUser.isAvatarImageSet) {
+            const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+            setContacts(data.data);
+          } else {
+            navigate("/setAvatar");
+          }
+        }
+      }, [currentUser]);
+
+    return (
+    <Container>
+        <div className="container">
+            <Contacts contacts={contacts} currentUser={currentUser} />
+        </div>
+    </Container>
+    );
 }
 
 export default Chat
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
